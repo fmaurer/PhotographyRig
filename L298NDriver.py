8d18ed375e9efb4b948f8e2585984bb511ec09a0
Florian <florianmaurer@gmail.com>
2024-05-25 14:55:04 -0700

    Initial degrees to stepper counts implementation

--- L298NDriver.py
+++ L298NDriver.py
@@ -7,9 +7,24 @@ class L298NDriver(MotorDriver):
         # motor_pins should be a list of pin numbers
         self.motor_devices = [OutputDevice(pin) for pin in motor_pins]
 
+    def num_steps_for_angle(self, degrees):
+        stepper_step_size = 1.8 #degrees
+        driver_gear_teeth = 12
+        bevel_gear_teeth = 12
+        ring_gear_teeth = 48
+
+        stage_0 = bevel_gear_teeth / driver_gear_teeth
+        stage_1 = ring_gear_teeth / bevel_gear_teeth
+
+        reduction = stage_1/stage_0
+
+        return round(stepper_step_size / reduction)
+
+
     def step_to_angle(self, degrees):
-        #TODO: Implement drive stepper driver to reach the full assembly's final degree of pan/tilt motors
-        self.step_motor(200, 0.005)
+        #self.step_motor(self.num_steps_for_angle(degrees), 0.005)
+        #-------DANGER ONLY USE ABOVE WHEN SAFETIES AND SHUTOFFS IMPLEMENTED :: HAS NOT BEEN TESTED------:
+        self.step_motor(50, 0.005) #200 steps for full revolution
         return 0
 
     def step_motor(self, steps, step_delay):
