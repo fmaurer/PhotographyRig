5afbe1b71185cde837a32c8aa54478551161c4dc
Florian <florianmaurer@gmail.com>
2025-06-29 21:56:10 -0700

    Add quick temp reading shell script

---
+++ temp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
+cpuTemp1=$(($cpuTemp0/1000))
+cpuTemp2=$(($cpuTemp0/100))
+cpuTempM=$(($cpuTemp2 % $cpuTemp1))
+
+gpuTemp0=$(vcgencmd measure_temp)
+gpuTemp0=${gpuTemp0//\'/º}
+gpuTemp0=${gpuTemp0//temp=/}
+
+echo CPU Temp: $cpuTemp1"."$cpuTempM"ºC"
+echo GPU Temp: $gpuTemp0
